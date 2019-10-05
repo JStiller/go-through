d33cf1bc011044432fb3a6141afa0bf7dfa107af
José Stiller <jstiller@users.noreply.github.com>
2019-10-05 14:29:15 +0200

    How to write a condition within a for loop

--- main.go
+++ main.go
@@ -12,32 +12,13 @@ func main() {
 	var b int = 1
 	fmt.Println(b)
 
-	// Using shorthand for declaring and initializing a variable
-	i := 1
-
-	// Writing a simple for loop
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
-	}
-
-	// Writing a classic intial (j), condition (j > 0) and after (j--) for loop
-	for j := 10; j > 0; j-- {
-		fmt.Println(j)
-	}
-
-	// Wirting an for loop without any condition. This loop will loop repeatedly until you break out of the loop
-	for {
-		fmt.Println("break loop")
-		break
-	}
-
-	// Writing an for loop with an continue. The counterpart of an break
-	for odd := 0; odd <= 5; odd++ {
-		if odd%2 == 0 {
+	// Writing an for loop with by using an shorthand for n
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			fmt.Println(n, "is even")
 			continue
 		}
 
-		fmt.Println(odd)
+		fmt.Println(n, "is odd")
 	}
 }
