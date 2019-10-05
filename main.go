4d03457255dff30fb443d4a1ea18ece5fe6dbff3
José Stiller <jstiller@users.noreply.github.com>
2019-10-05 14:13:04 +0200

    How to set variables

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+	// How to set variables
+	var a = "hello world!"
+	fmt.Println(a)
+
+	var b int = 1
+	fmt.Println(b)
+
+	var c = true
+	fmt.Println(c)
+
+	var d int
+	fmt.Println(d)
+
+	// using shorthand for declaring and initializing a variable
+	e := "hello world!"
+	fmt.Println(e)
+
+	f := 1
+	fmt.Println(f)
+
+	g := true
+	fmt.Println(g)
 }
